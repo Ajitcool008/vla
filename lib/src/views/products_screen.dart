9fa8d512ef5724369abdf026b43278968b890a2d
DellPC <ajit.cool008@gmail.com>
2022-07-09 12:10:05 +0530

    vector image product screen change

--- lib/src/views/products_screen.dart
+++ lib/src/views/products_screen.dart
@@ -25,10 +25,11 @@ class ProductScreen extends StatelessWidget {
         backgroundColor: Color(0xff0EBE7F),
         automaticallyImplyLeading: false,
         centerTitle: true,
-        title: const Text(
+        title: const Center(
+            child: const Text(
           'Product',
           style: TextStyle(fontSize: 23),
-        ),
+        )),
         actions: <Widget>[
           Padding(
             padding: const EdgeInsets.all(10.0),
@@ -88,10 +89,9 @@ class ProductScreen extends StatelessWidget {
             height: Get.height * 0.06,
             // width: Get.width * 0.999,
             padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(14, 190, 127, 89),
-              border: Border.all(color: Colors.white, width: 2),
-              //borderRadius: BorderRadius.circular(15)
+            decoration:  BoxDecoration(color: Color.fromRGBO(14, 190, 127, 89),
+            border: Border.all(color: Colors.white,width: 2),
+            //borderRadius: BorderRadius.circular(15)
             ),
             child: Row(
               //mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -101,21 +101,21 @@ class ProductScreen extends StatelessWidget {
                   style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: Get.width * 0.06),
+                  padding:  EdgeInsets.only(left: Get.width*0.06),
                   child: const Text(
                     'Price',
                     style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: Get.width * 0.12),
+                  padding:  EdgeInsets.only(left: Get.width*0.12),
                   child: const Text(
                     'Ton',
                     style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: Get.width * 0.14),
+                  padding:  EdgeInsets.only(left: Get.width*0.14),
                   child: const Text(
                     'Piece',
                     style: TextStyle(color: Colors.white, fontSize: 18),
@@ -181,7 +181,7 @@ class ProductScreen extends StatelessWidget {
     return Obx(
       () => Expanded(
           child: ListView.builder(
-              // padding: const EdgeInsets.all(8),
+             // padding: const EdgeInsets.all(8),
               // separatorBuilder: (BuildContext context, int index) => SizedBox(
               //   height: Get.height * 0.02,
               // ),
@@ -190,7 +190,7 @@ class ProductScreen extends StatelessWidget {
                 return ClipRRect(
                   //borderRadius: BorderRadius.all(Radius.circular(10)),
                   child: Container(
-                    height: Get.height * 0.1,
+                    height: Get.height*0.1,
                     decoration: BoxDecoration(
                       color: colorCodes[index],
                       //borderRadius: const BorderRadius.all(Radius.circular(4)),
@@ -224,12 +224,10 @@ class ProductScreen extends StatelessWidget {
                         // ),
                         Column(
                           children: [
-                            SizedBox(
-                              height: Get.height * 0.012,
-                            ),
+                            SizedBox(height: Get.height*0.012,),
                             Text(
                               'Starts From',
-                              style: TextStyle(color: Color(0xff0EBE7F), fontSize: 10, fontWeight: FontWeight.w800),
+                              style: TextStyle(color:Color(0xff0EBE7F),fontSize: 10, fontWeight: FontWeight.w800),
                             ),
                             Text(
                               con.products[index].price,
@@ -241,6 +239,7 @@ class ProductScreen extends StatelessWidget {
                             height: 40,
                             width: 70,
                             child: TextField(
+                                keyboardType: TextInputType.number,
                                 style: TextStyle(color: Colors.black),
                                 controller: con.products[index].ton,
                                 decoration: InputDecoration(
@@ -249,65 +248,73 @@ class ProductScreen extends StatelessWidget {
                             height: 40,
                             width: 70,
                             child: TextField(
+                                keyboardType: TextInputType.number,
                                 style: TextStyle(color: Colors.black),
                                 controller: con.products[index].piece,
                                 decoration: InputDecoration(
                                     enabledBorder: decoration, focusedBorder: decoration, border: decoration))),
                         InkWell(
-                            onTap: () async {
-                              if (con.products[index].piece.text.isEmpty && con.products[index].ton.text.isEmpty) {
-                                AppSnackbar.showSnackbar("INFO", 'Enter some value', AlertType.info);
-                              } else {
-                                var checkPin = await UtilityHelper().getPincodeCheck(
-                                    pincode: AppManager.loginHelper.restoreStudentListModel().data?.pincode ?? "");
-                                if (checkPin) {
-                                  var dataSet = {
-                                    "operation": "ProductPrice",
-                                    "Productid": con.products[index].productID,
-                                    "InTon": con.products[index].ton.text,
-                                    "Pcs": con.products[index].piece.text
-                                  };
-                                  var productPriceData = await con.getProductVal(input: dataSet);
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: Text(productPriceData.name),
-                                        content: SizedBox(
-                                          height: 80,
-                                          child: Column(
-                                            mainAxisAlignment: MainAxisAlignment.start,
-                                            children: [
-                                              Text("Product Weight : " + productPriceData.calculatedWeight),
-                                              Text("Product Price : " + productPriceData.calculatedPrice.toString()),
-                                            ],
-                                          ),
+                          onTap: () async {
+                            if (con.products[index].piece.text.isEmpty && con.products[index].ton.text.isEmpty) {
+                              AppSnackbar.showSnackbar("INFO", 'Enter some value', AlertType.info);
+                            } else {
+                              var checkPin = await UtilityHelper().getPincodeCheck(
+                                  pincode: AppManager.loginHelper.restoreStudentListModel().data?.pincode ?? "");
+                              if (checkPin) {
+                                var dataSet = {
+                                  "operation": "ProductPrice",
+                                  "Productid": con.products[index].productID,
+                                  "InTon": con.products[index].ton.text,
+                                  "Pcs": con.products[index].piece.text
+                                };
+                                var productPriceData = await con.getProductVal(input: dataSet);
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.all(Radius.circular(32.0))),
+                                      title: Container(
+                                        width: Get.width*0.8,
+                                        color: Color(0xff0EBE7F),
+                                          child: Text(productPriceData.name)),
+                                      content: Container(
+                                        height: 80,
+                                        child: Column(
+                                          //mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Text("Product Weight : " + productPriceData.totalWeight.toString()),
+                                            Text("Product Price : " + productPriceData.totalPices.toString()),
+                                            Text("Calculated Price : " + productPriceData.calculatedPrice.toString()),
+                                          ],
                                         ),
-                                        actions: [
-                                          TextButton(
-                                            child: Text("CANCEL"),
-                                            onPressed: () {
-                                              Get.back();
-                                            },
-                                          ),
-                                          TextButton(
-                                            child: Text("CONTINUE"),
-                                            onPressed: () {
-                                              con.addCart(item: productPriceData);
-                                              Get.back();
-                                            },
-                                          )
-                                        ],
-                                      );
-                                      ;
-                                    },
-                                  );
-                                } else {
-                                  AppSnackbar.showSnackbar("INFO", 'Pincode issue', AlertType.info);
-                                }
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          child: Text("CANCEL"),
+                                          onPressed: () {
+                                            Get.back();
+                                          },
+                                        ),
+                                        TextButton(
+                                          child: Text("CONTINUE"),
+                                          onPressed: () {
+                                            con.addCart(item: productPriceData);
+                                            Get.back();
+                                          },
+                                        )
+                                      ],
+                                    );
+                                    ;
+                                  },
+                                );
+                              } else {
+                                AppSnackbar.showSnackbar("INFO", 'Pincode issue', AlertType.info);
                               }
-                            },
-                            child: Icon(Icons.add_shopping_cart_outlined)),
+                            }
+                          },
+                          child: Icon(Icons.add_shopping_cart_outlined)
+                        ),
                       ],
                     ),
                   ),
